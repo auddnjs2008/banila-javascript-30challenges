dba9ab251aeecab0e8441117c036d09a80144ac4
auddnjs2008 <46766443+auddnjs2008@users.noreply.github.com>
2020-07-02 21:45:24 +0900

    video player

--- Video Player/index.js
+++ Video Player/index.js
@@ -19,6 +19,38 @@ function playButton() {
   }
 }
 
-function volumeControl() {}
+function skip() {
+  const value = parseFloat(this.dataset.skip);
+  video.currentTime += value;
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
 
 /*Hook up the event listenrs*/
+
+video.addEventListener("timeupdate", handleProgress);
+toggle.addEventListener("click", playButton);
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+let mousedown = false;
+
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
